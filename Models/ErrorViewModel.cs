4bb875584c03a93aba98ea49882af4e348bcaa72
sowmyarao28 <sowmyarao28@gmail.com>
2024-08-07 14:21:24 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DeploymentCenterApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
